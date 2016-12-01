9ca51f91d99bb23ab7e97d69ba9fadc6a69c9172
izhun <igor.zhun@gmail.com>
2016-12-02 01:04:05 +0200

    Update exercise_21.py

--- lesson_02/exercise_21.py
+++ lesson_02/exercise_21.py
@@ -6,8 +6,8 @@ import math
 a = float(sys.argv[1])
 b = float(sys.argv[2])
 
-ch = math.sqrt (a * b)
-zn = math.exp (a) * b
+ch = math.sqrt(a * b)
+zn = math.exp(a) * b
 dod = a * math.exp(2 * a / b)
 
 print(ch / zn + dod)
